a03d537d801918bc6b156c859dc1a3e7bcdf53d0
Hughes <codedpiano@gmail.com>
2021-04-04 14:17:42 -0400

    create single permission command

--- cogs/settings.py
+++ cogs/settings.py
@@ -57,7 +57,7 @@ class Settings(commands.Cog):
 
 
     @commands.command()
-    async def setpermission(self, ctx, role_id, permission_level):
+    async def permissions(self, ctx, action, role_id, permission_level=0):
         # level 10 command
         if not check(ctx, 10):
             raise Exception("Missing permissions")
@@ -65,74 +65,63 @@ class Settings(commands.Cog):
         try:
             role_id = int(role_id)
             permission_level = int(permission_level)
-            if permission_level > 10 or permission_level < 1:
-                raise ValueError("Invalid input")
         except:
             raise ValueError("Invalid input")
-        query = f"""
-        INSERT INTO permissions (role_id, permission_level)
-        VALUES ({role_id}, {permission_level});
-        """
-        try:
+        if permission_level > 10 or (permission_level < 0 and not action == "get"):
+            raise ValueError("Invalid input for permission_level")
+        if action not in ['set', 'update', 'remove', 'get']:
+            raise ValueError("Invalid action")
+        for server in self.bot.guilds:
+            try:
+                role = server.get_role(role_id)
+            except:
+                pass
+        if role:
+            await ctx.send(f"Role name: {role}")
+        else:
+            await ctx.send("Role name not found")
+
+        if action == "set":
+            query = f"""
+                INSERT INTO permissions (role_id, permission_level)
+                VALUES ({role_id}, {permission_level});
+            """
+            try:
+                execute_query('databases/permissions.sqlite', query)
+            except Exception as e:
+                if "UNIQUE" in str(e):
+                    await ctx.send("Use `updatepermission` instead")
+                    return
+            await ctx.send(f"Set {role_id} permission level to {permission_level}")
+        elif action == "update":
+            query = f"""
+                UPDATE permissions
+                SET permission_level = {permission_level}
+                WHERE role_id = {role_id};
+            """
             execute_query('databases/permissions.sqlite', query)
-        except Exception as e:
-            if "UNIQUE" in str(e):
-                await ctx.send("Use `updatepermission` instead")
+            await ctx.send(f"Updated {role_id} permission level to {permission_level}")
+        elif action == "remove":
+            query = f"""
+                DELETE FROM permissions
+                WHERE role_id = {role_id};
+            """
+            execute_query('databases/permissions.sqlite', query)
+            await ctx.send(f"Removed permissions entry for {role_id}")
+        elif action == "get":
+            query = f"SELECT * FROM permissions WHERE role_id = {role_id}"
+            result = read_query('databases/permissions.sqlite', query)
+            try:
+                entry = result[0]
+            except IndexError:
+                await ctx.send(f"Permissions entry for {role_id} not found")
                 return
-        await ctx.send(f"Set {role_id} permission level to {permission_level}")
+            await ctx.send(f"Role {entry[0]} has a permission level of {entry[1]}")
 
-    @setpermission.error
-    async def setpermission_error(self, ctx, error):
+    @permissions.error
+    async def permissions_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"Missing argument, correct syntax is `{self.bot.command_prefix}setpermission <role_id> <permission_level>`")
-
-
-    @commands.command()
-    async def updatepermission(self, ctx, role_id, permission_level):
-        # level 10 command
-        if not check(ctx, 10):
-            raise Exception("Missing permissions")
-            return
-        try:
-            role_id = int(role_id)
-            permission_level = int(permission_level)
-            if permission_level > 10 or permission_level < 1:
-                raise ValueError("Invalid input")
-        except:
-            raise ValueError("Invalid input")
-        query = f"""
-        UPDATE permissions
-        SET permission_level = {permission_level}
-        WHERE role_id = {role_id};
-        """
-        execute_query('databases/permissions.sqlite', query)
-        await ctx.send(f"Updated {role_id} permission level to {permission_level}")
-
-    @updatepermission.error
-    async def updatepermission_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"Missing argument, correct syntax is `{self.bot.command_prefix}updatepermission <role_id> <permission_level>`")
-
-    
-    @commands.command()
-    async def getpermission(self, ctx, role_id):
-        # level 6 command
-        if not check(ctx, 6):
-            raise Exception("Missing permissions")
-            return
-        try:
-            role_id = int(role_id)
-        except:
-            raise ValueError("Invalid input")
-        query = f"SELECT * FROM permissions WHERE role_id = {role_id}"
-        result = read_query('databases/permissions.sqlite', query)
-        entry = result[0]
-        await ctx.send(f"Role {entry[0]} has a permission level of {entry[1]}")
-
-    @getpermission.error
-    async def getpermission_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"Missing argument, correct syntax is `{self.bot.command_prefix}getpermission <role_id>`")
+            await ctx.send(f"Missing argument, correct syntax is `{self.bot.command_prefix}permissions <action> <role_id> [permission_level]`")
 
 
     @commands.command()
