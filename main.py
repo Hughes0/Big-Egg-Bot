a9538a7b4a9fd7893bc191fae344da3ec0ee92cc
Hughes <codedpiano@gmail.com>
2021-04-04 12:01:13 -0400

    permissions db initialization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import helpers
 import os
 import discord
 from discord.ext import commands
+from cogs import settings
 
 
 bot = commands.Bot(command_prefix = "$")
@@ -10,11 +11,13 @@ bot = commands.Bot(command_prefix = "$")
 
 @bot.command()
 async def online(ctx):
+    # command to test if bot is running
     await ctx.send(f"Bot is online with latency {round(bot.latency, 3)}")
 
 
 @bot.event
 async def on_command_error(ctx, error):
+    # argument errors are handled function-specifically
     if isinstance(error, commands.MissingRequiredArgument):
         pass
     elif isinstance(error,commands.CheckFailure):
@@ -32,6 +35,14 @@ async def on_ready():
     # set bot activity to help command
     game = discord.Game(f"{bot.command_prefix}help")
     await bot.change_presence(activity=game)
+    # create permissions table
+    create_permissions_table = """
+    CREATE TABLE IF NOT EXISTS permissions (
+        role_id INTEGER PRIMARY KEY,
+        permission_level INTEGER NOT NULL
+    );
+    """
+    settings.execute_query('databases/permissions.sqlite', create_permissions_table)
   
 
 
@@ -40,7 +51,7 @@ print("Loading cogs...")
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-        print(filename)
+        print(f"- {filename}")
 print("Done!")
 
 
