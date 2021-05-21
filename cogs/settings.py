3ec5305bb7ee98fd31210d76e63b1f6d06f26786
Hughes <codedpiano@gmail.com>
2021-05-21 13:43:41 -0400

    refactor apikey command to use subcommands

--- cogs/settings.py
+++ cogs/settings.py
@@ -125,75 +125,64 @@ class Settings(commands.Cog):
             await ctx.send(f"Missing argument, correct syntax is `{self.bot.command_prefix}testcheck <level>`")
 
 
-    @commands.command()
-    async def apikey(self, ctx, action, owner=None, key=None):
+    @commands.group(pass_context=True)
+    async def apikey(self, ctx):
         # level 10 command
         helpers.check(ctx, 10)
-        if not action:
-            raise ValueError("Invalid action, options are `set`, `get`, `update`, and `remove`")
-        if action == "set":
-            if not owner or not key:
-                raise ValueError(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey set <owner> <key>`")
-            owner = owner.lower()
-            query = """
-                INSERT INTO keys
-                    (key, owner, alliance_id, alliance_position, requests_remaining)
-                VALUES
-                    (?, ?, ?, ?, ?);
-            """
-            url = f"https://politicsandwar.com/api/v2/nations/{key}/&alliance_id=7450&alliance_position=2,3,4,5&v_mode=false"
-            data = requests.get(url).json()
-            helpers.catch_api_error(data, 2)
-            data = data['api_request']['api_key_details']
-            arguments = (key, owner, data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'])
-            helpers.execute_query('databases/keys.sqlite', query, arguments)
-            await ctx.send(f"Saved API key entry for `{owner}` to database")
-        elif action == "get":
-            if not owner:
-                raise ValueError(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey get <owner>`")
-            if owner == "all":
-                query = "SELECT * FROM keys"
-                result = helpers.read_query('databases/keys.sqlite', query)
+        # case no action selected
+        if ctx.invoked_subcommand is None:
+            raise Exception("Invalid action")
+
+    @apikey.command(pass_context=True)
+    async def setkey(self, ctx, owner, key):
+        # add api key entry
+        if not owner or not key:
+            raise Exception(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey set <owner> <key>`")
+        owner = owner.lower()
+        query = """
+            INSERT INTO keys
+                (key, owner, alliance_id, alliance_position, requests_remaining)
+            VALUES
+                (?, ?, ?, ?, ?);
+        """
+        url = f"https://politicsandwar.com/api/v2/nations/{key}/&alliance_id=7450&alliance_position=2,3,4,5&v_mode=false"
+        data = requests.get(url).json()
+        helpers.catch_api_error(data, 2)
+        data = data['api_request']['api_key_details']
+        arguments = (key, owner, data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'])
+        helpers.execute_query('databases/keys.sqlite', query, arguments)
+        await ctx.send(f"Saved API key entry for `{owner}` to database")
+
+    @apikey.command(pass_context=True)
+    async def getkey(self, ctx, owner):
+        # get api key entry
+        if not owner:
+            raise Exception(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey get <owner>`")
+        if owner == "all":
+            query = "SELECT * FROM keys"
+            result = helpers.read_query('databases/keys.sqlite', query)
+            await ctx.send(result)
+        else:
+            query = "SELECT * FROM keys WHERE owner = ?"
+            result = helpers.read_query('databases/keys.sqlite', query, (owner,))
+            if not result:
+                await ctx.send(f"No entry found for {owner}")
+            else:
                 await ctx.send(result)
-            else:
-                query = "SELECT * FROM keys WHERE owner = ?"
-                result = helpers.read_query('databases/keys.sqlite', query, (owner,))
-                if not result:
-                    await ctx.send(f"No entry found for {owner}")
-                else:
-                    await ctx.send(result)
-        elif action == "update":
-            if not owner:
-                raise ValueError(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey update <owner>`")
-            if owner == "all":
-                query = "SELECT owner, key FROM keys"
-                result = helpers.read_query('databases/keys.sqlite', query)
-                if not result:
-                    raise Exception("No API key entries found")
-                owners = [entry[0] for entry in result]
-                keys = [entry[1] for entry in result]
-                for i in range(len(owners)):
-                    query = """
-                        UPDATE keys
-                        SET alliance_id = ?,
-                            alliance_position = ?,
-                            requests_remaining = ?
-                        WHERE owner = ?;
-                    """
-                    url = f"https://politicsandwar.com/api/v2/nations/{keys[i]}/&alliance_id=7450&alliance_position=2,3,4,5&v_mode=false"
-                    data = requests.get(url).json()
-                    helpers.catch_api_error(data, 2)
-                    data = data['api_request']['api_key_details']
-                    arguments = (data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'], owners[i])
-                    helpers.execute_query('databases/keys.sqlite', query, arguments)
-                    await ctx.send(f"Updated `{owners[i]}` API key entry")
-                await ctx.send("Updated all API key entries")
-            else:
-                query = "SELECT key FROM keys WHERE owner = ?"
-                result = helpers.read_query('databases/keys.sqlite', query, (owner,))
-                if not result:
-                    raise Exception(f"API key entry for `{owner}` not found")
-                key = result[0][0]
+
+    @apikey.command(pass_context=True)
+    async def updatekey(self, ctx, owner):
+        # update apikey data (allianceposition, requests remaining)
+        if not owner:
+            raise Exception(f"Missing arguments, correct syntax is `{self.bot.command_prefix}apikey update <owner>`")
+        if owner == "all":
+            query = "SELECT owner, key FROM keys"
+            result = helpers.read_query('databases/keys.sqlite', query)
+            if not result:
+                raise Exception("No API key entries found")
+            owners = [entry[0] for entry in result]
+            keys = [entry[1] for entry in result]
+            for i in range(len(owners)):
                 query = """
                     UPDATE keys
                     SET alliance_id = ?,
@@ -201,22 +190,45 @@ class Settings(commands.Cog):
                         requests_remaining = ?
                     WHERE owner = ?;
                 """
-                url = f"https://politicsandwar.com/api/v2/nations/{key}/&alliance_id=7450&alliance_position=2,3,4,5&v_mode=false"
+                url = f"https://politicsandwar.com/api/v2/nations/{keys[i]}/&alliance_id=7450&alliance_position=5&v_mode=false"
                 data = requests.get(url).json()
                 helpers.catch_api_error(data, 2)
                 data = data['api_request']['api_key_details']
-                arguments = (data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'], owner)
+                arguments = (data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'], owners[i])
                 helpers.execute_query('databases/keys.sqlite', query, arguments)
-                await ctx.send(f"Updated `{owner}` API key entry")
-        elif action == "remove":
+                await ctx.send(f"Updated `{owners[i]}` API key entry")
+            await ctx.send("Updated all API key entries")
+        else:
+            query = "SELECT key FROM keys WHERE owner = ?"
+            result = helpers.read_query('databases/keys.sqlite', query, (owner,))
+            if not result:
+                raise Exception(f"API key entry for `{owner}` not found")
+            key = result[0][0]
             query = """
-                DELETE FROM keys
+                UPDATE keys
+                SET alliance_id = ?,
+                    alliance_position = ?,
+                    requests_remaining = ?
                 WHERE owner = ?;
             """
-            helpers.execute_query('databases/keys.sqlite', query, (owner,))
-            await ctx.send(f"Removed API key entry for `{owner}`")
-        else:
-            raise ValueError("Invalid action, options are `set`, `get`, `update`, and `remove`")
+            url = f"https://politicsandwar.com/api/v2/nations/{key}/&alliance_id=7450&alliance_position=5&v_mode=false"
+            data = requests.get(url).json()
+            helpers.catch_api_error(data, 2)
+            data = data['api_request']['api_key_details']
+            arguments = (data['alliance_id'], data['alliance_position'], data['daily_requests_remaining'], owner)
+            helpers.execute_query('databases/keys.sqlite', query, arguments)
+            await ctx.send(f"Updated `{owner}` API key entry")
+    
+    @apikey.command(pass_context=True)
+    async def removekey(self, ctx, owner):
+        # remove api key entry
+        query = """
+            DELETE FROM keys
+            WHERE owner = ?;
+        """
+        helpers.execute_query('databases/keys.sqlite', query, (owner,))
+        await ctx.send(f"Removed API key entry for `{owner}`")
+
 
     @apikey.error
     async def apikey_error(self, ctx, error):
